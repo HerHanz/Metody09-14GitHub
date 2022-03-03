c3d459515bd1393c1cd774b7048574f4fa3441de
Scitani <havelkaj.04@A321-11>
2022-03-03 13:18:25 +0100

    Přidejte soubory projektu.

---
+++ Metody09-14GitHub/Vypocty.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metody09_14GitHub
+{
+    class Vypocty
+    {
+        private bool KvadRovnice(double a,double b,double c,out double diskriminant,out double x1,out double x2 )
+        {
+
+            return ();
+        }
+
+
+
+
+    }
+}

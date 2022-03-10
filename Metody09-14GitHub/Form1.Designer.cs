f18c5b417bdc2e813c3d78ffd5322599412985d4
Jan Havelka <havelkaj.04@spst.cz>
2022-03-10 19:12:30 +0100

    Alfanum a Obsahuje slovo

--- Metody09-14GitHub/Form1.Designer.cs
+++ Metody09-14GitHub/Form1.Designer.cs
@@ -36,13 +36,18 @@
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(10, 35);
+            this.button1.Location = new System.Drawing.Point(13, 43);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(127, 35);
+            this.button1.Size = new System.Drawing.Size(169, 43);
             this.button1.TabIndex = 0;
             this.button1.Text = "Vypocet Kvadraticke rovnice";
             this.button1.UseVisualStyleBackColor = true;
@@ -50,34 +55,37 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(183, 20);
+            this.textBox1.Location = new System.Drawing.Point(244, 25);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(94, 20);
+            this.textBox1.Size = new System.Drawing.Size(124, 22);
             this.textBox1.TabIndex = 1;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(183, 64);
+            this.textBox2.Location = new System.Drawing.Point(244, 79);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(94, 20);
+            this.textBox2.Size = new System.Drawing.Size(124, 22);
             this.textBox2.TabIndex = 2;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(183, 108);
+            this.textBox3.Location = new System.Drawing.Point(244, 133);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(94, 20);
+            this.textBox3.Size = new System.Drawing.Size(124, 22);
             this.textBox3.TabIndex = 3;
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(28, 170);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Location = new System.Drawing.Point(37, 209);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(101, 37);
+            this.button2.Size = new System.Drawing.Size(135, 46);
             this.button2.TabIndex = 4;
             this.button2.Text = "ObsahujeCislo";
             this.button2.UseVisualStyleBackColor = true;
@@ -85,34 +93,76 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(183, 188);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox4.Location = new System.Drawing.Point(244, 231);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(76, 20);
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
             this.textBox4.TabIndex = 5;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(177, 274);
+            this.textBox5.Location = new System.Drawing.Point(236, 337);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.Size = new System.Drawing.Size(132, 22);
             this.textBox5.TabIndex = 6;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(28, 274);
+            this.button3.Location = new System.Drawing.Point(37, 337);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(81, 33);
+            this.button3.Size = new System.Drawing.Size(108, 41);
             this.button3.TabIndex = 7;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(701, 63);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(692, 157);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(850, 63);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 10;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(850, 157);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 22);
+            this.textBox7.TabIndex = 11;
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox4);
@@ -121,6 +171,7 @@
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -138,6 +189,10 @@
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
     }
 }
 
--- Metody09-14GitHub/Form1.cs
+++ Metody09-14GitHub/Form1.cs
@@ -19,17 +19,17 @@ namespace Metody09_14GitHub
 
         private void button1_Click(object sender, EventArgs e)
         {
-           
-                double a = double.Parse(textBox1.Text);
-                double b = double.Parse(textBox2.Text);
-                double c = double.Parse(textBox3.Text);
-                double x1, x2, d;
-                Vypocty.Diskriminant(a, b, c, out x1, out x2, out d);
-                if (d < 0) MessageBox.Show("Rovnice nema reseni v R");
-                else if (d > 0) MessageBox.Show("Koren x1 je: " + x1 + " a koren x2 je: " + x2);
-                else MessageBox.Show("Rovnice ma jedno dvojnasobne reseni " + x1);
 
-            
+            double a = double.Parse(textBox1.Text);
+            double b = double.Parse(textBox2.Text);
+            double c = double.Parse(textBox3.Text);
+            double x1, x2, d;
+            Vypocty.Diskriminant(a, b, c, out x1, out x2, out d);
+            if (d < 0) MessageBox.Show("Rovnice nema reseni v R");
+            else if (d > 0) MessageBox.Show("Koren x1 je: " + x1 + " a koren x2 je: " + x2);
+            else MessageBox.Show("Rovnice ma jedno dvojnasobne reseni " + x1);
+
+
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -53,12 +53,12 @@ namespace Metody09_14GitHub
             int cifsoucet = 0;
             int lichsoucet = 0;
             int sudcifra = 0;
-            if (Vypocty.ObsahujeCislici(a, out cifsoucet, out lichsoucet, out sudcifra)) 
+            if (Vypocty.ObsahujeCislici(a, out cifsoucet, out lichsoucet, out sudcifra))
             {
                 MessageBox.Show("Obsahuje cislici");
-                MessageBox.Show("Ciferny soucet je"+cifsoucet);
-                MessageBox.Show("Ciferny soucet lich"+lichsoucet);
-                MessageBox.Show("CIferny couset sud cisel je" +sudcifra);
+                MessageBox.Show("Ciferny soucet je" + cifsoucet);
+                MessageBox.Show("Ciferny soucet lich" + lichsoucet);
+                MessageBox.Show("CIferny couset sud cisel je" + sudcifra);
 
             }
             else MessageBox.Show("Neobsahuje cifru");
@@ -68,8 +68,8 @@ namespace Metody09_14GitHub
         private void button3_Click(object sender, EventArgs e)
         {
             string a = textBox5.Text;
-            string UpravenyRetezec = " " ;
-            MessageBox.Show("Pocetslov je" + Vypocty.PocetSlov1(a,out  UpravenyRetezec));
+            string UpravenyRetezec = " ";
+            MessageBox.Show("Pocetslov je" + Vypocty.PocetSlov1(a, out UpravenyRetezec));
 
 
 
@@ -77,5 +77,54 @@ namespace Metody09_14GitHub
 
 
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string s = textBox6.Text;
+            string nejdelsislovo, nejkratsislovo;
+            if (Vypocty.ObsahujeSlovo(s, out nejdelsislovo, out nejkratsislovo))
+            {
+                MessageBox.Show("Obsahuje více než jedno slovo");
+                MessageBox.Show("Nejdelší slovo " + nejdelsislovo);
+                MessageBox.Show("Nejkratší slovo " + nejkratsislovo);
+            }
+            else
+            {
+                MessageBox.Show("Řetězec neobsahuje ani jedno slovo");
+            }
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string s = textBox7.Text;
+            int pocetVelkych, pocetMalych, pocetJinychZnaku;
+            if (Vypocty.JeAlfanum(s, out pocetMalych, out pocetVelkych, out pocetJinychZnaku))
+            {
+                MessageBox.Show("Je alfanumerický");
+                MessageBox.Show("Počet malých: " + pocetMalych);
+                MessageBox.Show("Počet velkých: " + pocetVelkych);
+                MessageBox.Show("Počet jiných " + pocetJinychZnaku);
+            }
+            else
+            {
+                MessageBox.Show("Není alfanumerický");
+                MessageBox.Show("Počet malých: " + pocetMalych);
+                MessageBox.Show("Počet velkých: " + pocetVelkych);
+                MessageBox.Show("Počet jiných " + pocetJinychZnaku);
+
+            }
+        }
+    
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
-}
+    }
+
